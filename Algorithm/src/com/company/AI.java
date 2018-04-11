2b1894f705ca6b6f277586f6d1e18c1ca40e628d
Aleksey Shiyanov <Shiyanov1998@mail.ru>
2018-04-11 23:25:05 +0400

    Добавление анализа доски и выбора фигур способных бить в данный момент

--- Algorithm/src/com/company/AI.java
+++ Algorithm/src/com/company/AI.java
@@ -4,6 +4,11 @@ import java.awt.*;
 import java.util.ArrayList;
 
 public class AI {
+    /*
+     * если хотя бы одна шашка или дамка может бить, то выбираем битье при котором умрет наибольшее число вражеских фигур
+     * если при убийстве можем бить дамку, то убийство дамки преоритетнее убийства нескольких простых шашек
+     * при убийстве считать до тех пор пока можем бить
+     * */
 
     private int enemy;
     private int player;
@@ -16,32 +21,37 @@ public class AI {
         else if (player == 2) enemy = 1;
     }
 
-    public void scanBoard() {
-        int[][] a = GameBoard.board; // доска
-        ArrayList<Point> canChess = new ArrayList<>(); // список всех точек которыми может распоряжаться ИИ в данный момент времени
-        ArrayList<Point> canFightChess = mover.canFight(a); // список всех обычных шашек которые могут бить
-        ArrayList<Point> canFightCatChess = new ArrayList<>(); // список всех дамок которые могут бить в данный момент времени
+    public ArrayList<Point> canFightChees() {
+        int[][] a = GameBoard.board;
+        ArrayList<Point> canChess = new ArrayList<>(mover.canFight(a)); // список всех точек которыми может распоряжаться ИИ в данный момент времени
 
         // Ищем все дамки которые могут бить кого либо
         for (int i = 0; i < a.length; i++) {
             for (int j = 0; j < a[i].length; j++) {
                 if (a[i][j] < 0 && Math.abs(a[i][j]) == player && mover.canFightCat(a, i, j, enemy).size() != 0) {
-                    canFightCatChess.add(new Point(i, j));
+                    canChess.add(new Point(i, j));
                 }
             }
         }
-
-        if (canFightCatChess.size() != 0 || canFightChess.size() != 0) {
-            /*
-             * если хотя бы одна шашка или дамка может бить, то выбираем битье при котором умрет наибольшее число вражеских фигур
-             * если при убийстве можем бить дамку, то убийство дамки преоритетнее убийства нескольких простых шашек
-             * при убийстве считать до тех пор пока можем бить
-             * */
-            canChess.addAll(canFightCatChess);
-            canChess.addAll(canFightChess);
-        } else {
-            // если ни кто не может бить, то ищем фигуры которые могут пойти
-
-        }
+        return canChess;
     }
+
+    public boolean canFight(ArrayList<Point> fightChess) {
+        return fightChess.size() != 0;
+    }
+
+    public ArrayList<Point> canMoveChess() {
+        int[][] a = GameBoard.board;
+        ArrayList<Point> canMove = new ArrayList<>();
+
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[0].length; j++) {
+                if (Math.abs(a[i][j]) == player) {
+
+                }
+            }
+        }
+        return canMove;
+    }
+
 }
